4260630a470ee4320cc43addda6f942ec37a9197
yusivacova <sivacovayulia@gmail.com>
2023-01-25 14:22:09 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,63 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const encodedLetter = [];
+
+    for (let i = 0; i < expr.length - 9; i+=10 ){
+        let counter = i + 10;
+        encodedLetter.push(expr.slice(i, counter))
+    }
+    
+    let  encodedLetterNoZero = [];
+    encodedLetter.forEach(function (item){
+        for (let i = 0; i < item.length; i++){
+            if (item[i] === '1') {
+                encodedLetterNoZero.push(item.slice(i));
+                break;
+            }
+            if (item[i] === '*') {
+                encodedLetterNoZero.push(item);
+                break;
+            }
+        }
+    })
+
+    const morseCodeExpr = encodedLetterNoZero.map(function(item){
+    let itemEncoded = '';
+
+    if (item[0] === '1'){
+        for ( let i = 0; i < item.length; i+= 2) {
+        if (item[i] === '1' && item[i + 1] === '0') {
+             itemEncoded += '.';
+        } else if (item[i] === '1' && item[i + 1] === '1') {
+            itemEncoded += '-';
+        } 
+    }
+    if (item[0] ==='*') {
+            itemEncoded += ' ';
+        }
+    }
+     item = itemEncoded;
+   
+         return item;
+    });
+
+    const resultSentence = morseCodeExpr.map(function(item){
+        
+        for (let key in MORSE_TABLE) {
+            let value = MORSE_TABLE[key];
+            if (item=== key) {
+                item = value;
+               }
+            }
+        
+        if (item === '') item = ' ';
+
+        return item;
+    });
+
+
+return  resultSentence.join('');
 }
 
 module.exports = {
